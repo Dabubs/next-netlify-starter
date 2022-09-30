0dfb9f3975d5a27b662ffc757bdc75d96802cb80
Dabubs <86854646+Dabubs@users.noreply.github.com>
2022-10-01 01:32:21 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,14 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <html style="height: 100%;"><head><meta name="viewport" content="width=device-width, minimum-scale=0.1"><title>3dgifmaker14645.gif (303×303)</title></head><body style="margin: 0px; background: #0e0e0e; height: 100%"><img style="display: block;-webkit-user-select: none;margin: auto;background-color: hsl(0, 0%, 90%);" src="https://media.discordapp.net/attachments/1012671893188399145/1019946000409960538/3dgifmaker14645.gif"></body></html>
-    <body style="margin: 0px; background: #0e0e0e; height: 100%"><img style="display: block;-webkit-user-select: none;margin: auto;background-color: hsl(0, 0%, 90%);" src="https://media.discordapp.net/attachments/1012671893188399145/1019946000409960538/3dgifmaker14645.gif"></body>
+    <html style="height: 100%;">
+      <head>
+        <meta name="viewport" content="width=device-width, minimum-scale=0.1">
+          <title>3dgifmaker14645.gif (303×303)</title>
+      </head >
+      <body style="margin: 0px; background: #0e0e0e; height: 100%">
+        <img style="display: block;-webkit-user-select: none;margin: auto;background-color: hsl(0, 0%, 90%);" src="https://media.discordapp.net/attachments/1012671893188399145/1019946000409960538/3dgifmaker14645.gif">
+          </body>
+  </html>
   )
 }
