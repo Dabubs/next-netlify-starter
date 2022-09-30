57adeb2d027c326b572cae760455b22d96b4b7f9
Dabubs <86854646+Dabubs@users.noreply.github.com>
2022-10-01 01:21:55 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-          <iframe src="https://media.discordapp.net/attachments/1012671893188399145/1019946000409960538/3dgifmaker14645.gif" height="500" width="700" title="Spinning Hottie"></iframe>
+          <iframe src="https://media.discordapp.net/attachments/1012671893188399145/1019946000409960538/3dgifmaker14645.gif" height="200" width="300" title="Spinning Hottie"></iframe>
       </main>
 
       <Footer />
